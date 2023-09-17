024a632cb899d887f27ca392788fb7af0a809d10
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-09-17 23:57:26 +0600

    Build Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Logo from "./Logo";
+import ProfileAvatar from "./ProfileAvatar";
+import SearchBox from "./SearchBox";
+import SearchLocation from "./SearchLocation";
+
+const Navbar = () => {
+  return (
+    <header className="w-full sticky top-0 px-4 lg:border-b border-0 bg-white z-40">
+      <nav className="h-14 lg:h-20  border-b lg:border-0 max-w-[1036px] mx-auto flex items-center">
+        <Logo />
+        <div className="hidden lg:flex">
+          <SearchLocation />
+        </div>
+
+        <div className="w-full hidden lg:flex">
+          <SearchBox />
+        </div>
+
+        <div className="ml-auto grid place-content-center">
+          <ProfileAvatar />
+        </div>
+      </nav>
+      <div className="max-w-[1036px] mx-auto py-2 flex flex-col gap-2 lg:hidden">
+        <SearchLocation />
+        <SearchBox />
+      </div>
+    </header>
+  );
+};
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        hero: "linear-gradient(0deg, rgb(201, 188, 244) 0%, rgb(255, 255, 255) 95.83%)",
+      },
+      boxShadow: {
+        input:
+          "rgba(27, 30, 36, 0.06) 0px 8px 16px, rgba(27, 30, 36, 0.04) 0px 0px 8px;",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
