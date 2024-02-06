867c5992812b5143084d4380d99e963f7e717e84
Anjan Karmaker <77603380+anjankarmakar10@users.noreply.github.com>
2024-02-06 11:10:16 +0600

    convert useRecipes to ReactQuery

--- app/components/DiscoverRecipes.tsx
+++ app/components/DiscoverRecipes.tsx
@@ -5,7 +5,9 @@ import RecipeCard from "./RecipeCard";
 import Skeleton from "./Skeleton";
 
 const DiscoverRecipes = () => {
-  const { recipes } = useRecipes();
+  const { data, isLoading } = useRecipes();
+
+  console.log(data?.recipes);
 
   return (
     <section className="max-w-[1036px] mx-auto px-4 ">
@@ -14,13 +16,13 @@ const DiscoverRecipes = () => {
       </header>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4  pb-8">
-        {recipes
-          ? recipes?.map((recipe) => (
-              <RecipeCard key={recipe.id} recipe={recipe} />
-            ))
-          : Array(10)
-              .fill("@")
-              .map((_, i) => <Skeleton key={i} />)}
+        {isLoading &&
+          Array(10)
+            .fill("@")
+            .map((_, i) => <Skeleton key={i} />)}
+        {data?.recipes.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        ))}
       </div>
     </section>
   );
--- app/hooks/useRecipes.tsx
+++ app/hooks/useRecipes.tsx
@@ -1,31 +1,20 @@
 import { API_KEY, BASE_URL } from "@/lib/constants";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+interface QueryResponse {
+  recipes: Recipe[];
+}
 
 const useRecipes = () => {
-  const [recipes, setRecipes] = useState<Recipe[] | null>(null);
-
-  const getData = async (signal: AbortSignal) => {
-    try {
-      const res = await fetch(
-        `${BASE_URL}/recipes/random?number=10&tags=vegetarian,chicken&apiKey=${API_KEY}`,
-        { signal }
-      );
-
-      const { recipes } = await res.json();
-      setRecipes(recipes);
-    } catch (error) {
-      console.log(error);
-    }
+  const getData = () => {
+    return fetch(
+      `${BASE_URL}/recipes/random?number=10&tags=vegetarian,chicken&apiKey=${API_KEY}`
+    ).then((res) => res.json());
   };
 
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    getData(signal);
-
-    return () => controller.abort();
-  }, []);
-
-  return { recipes };
+  return useQuery<QueryResponse>({
+    queryKey: ["recipes"],
+    queryFn: getData,
+  });
 };
 export default useRecipes;
